42bf6f671d11a00f6cad14ea2dd31dd7fe13f1fb
Jared Blackman <jared.blackman@blinq.me>
2022-06-20 11:13:07 +1000

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ImageEditor } from "blinq-image-editor";
+
+export function IndexPage() {
+  return (
+    <div>
+      <ImageEditor />
+    </div>
+  );
+}
+
+export default IndexPage;
