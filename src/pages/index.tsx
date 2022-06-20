79c7af4a2c6a9615be491fdf689e905398fd0ca4
Jared Blackman <jared.blackman@blinq.me>
2022-06-20 12:21:31 +1000

    CacheProvider attempt

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+import { CacheProvider } from "@emotion/react";
 import { ImageEditor } from "blinq-image-editor";
+import React from "react";
+import createCache from "@emotion/cache";
+
+const cache = createCache({
+  key: "xlgcyzurhl",
+});
 
 export function IndexPage() {
   return (
-    <div>
-      <ImageEditor />
-    </div>
+    <CacheProvider value={cache}>
+      <div>
+        <ImageEditor />
+      </div>
+    </CacheProvider>
   );
 }
 
